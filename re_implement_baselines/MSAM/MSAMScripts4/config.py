323c88dd96dda5d35409a79ca16265b2b6e75c29
ChTianlEI <148170275+ChTianlEI@users.noreply.github.com>
2026-04-07 23:19:25 +0800

    Add files via upload

---
+++ re_implement_baselines/MSAM/MSAMScripts4/config.py
@@ -0,0 +1,32 @@
+import torch 
+
+# ======================= Classification CONFIG ========================================
+
+DEVICE = torch.device('cuda')
+
+MODEL_TYPE = "CE_MSAM" #or "LE" or "CE" or "LE_MSAM" or "CE_MSAM" or "CE_MSAM_CLQ" or "LE_MSAM_CLQ"
+DATA_TYPE = "mimic4_icd9" #or "top50" or "clean"
+M = 4
+SELECTION_CRITERION = "MDP" #or 'MDP' or 'rand'
+
+FILE_NAME = "mimic4_icd9"
+
+START_MODEL_FROM_CHECKPOINT = ""
+
+#CLQ Arguments
+HUBER_DELTA = 0.5
+QUANT_LAMBDA = 100
+START_CLQ_FROM_CLASSIFIER_CHECKPOINT = ""
+START_CLQ_FROM_QUANTIFIER_CHECKPOINT = ""
+
+#Train/Test switches for the classifiers
+MODE = "train" #or "train" or "test" 
+
+# ===================================================================
+# DON'T CHANGE:
+PRETRAIN_MODEL = "/home/songjunru/.cache/huggingface/hub/models--UFNLP--gatortron-base/snapshots/78b867169b09bc34972c2c497aa323e94deb79d3/"
+
+DATA_PATH = "../sample_data/{}".format(DATA_TYPE)
+SAVE_METRICS_PATH = "../models/{}/{}/metrics".format(DATA_TYPE,FILE_NAME)
+SAVE_PREDICTION_PATH = "../models/{}/{}/predictions".format(DATA_TYPE,FILE_NAME)
+OUTPUT_DIR= "../models/{}/{}/model".format(DATA_TYPE,FILE_NAME)
---
+++ re_implement_baselines/MSAM/MSAMScripts4/config_quantification.py
@@ -0,0 +1,20 @@
+import torch 
+
+# ======================= CONFIG ========================================
+
+DEVICE = torch.device('cuda')
+
+MODEL_TYPE = "CE_MSAM_CLQ" #or "LE" or "CE" or "LE_MSAM" or "CE_MSAM" or "CE_MSAM_CLQ" or "LE_MSAM_CLQ"
+DATA_TYPE = "mimic4_icd9" #or "top50" or "clean"
+FILE_NAME = "test"
+
+MODE = "test"
+EPOCHS = 5
+PATIENCE = 5
+HUBER_DELTA = 0.5
+LR = 0.0002
+
+#DON'T CHANGE
+SAVE_METRICS_PATH = "../save/{}/{}/metrics".format(DATA_TYPE,FILE_NAME)
+SAVE_PREDICTION_PATH = "../save/{}/{}/predictions".format(DATA_TYPE,FILE_NAME)
+OUTPUT_DIR= "../save/{}/{}/model".format(DATA_TYPE,FILE_NAME)
